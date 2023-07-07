36e0cd4a0f9b95ec329a101c2574c6cff8838c7c
Anna <a.s.markovskaya@gmail.com>
2023-07-08 01:55:35 +0300

    1st draft

---
+++ task-36/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int ReadInt(string text) 
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] FillArrayRandom(int tmpLength, int tmpMin, int tmpMax)
+{
+    int[] tmpArray = new int[tmpLength];
+    Random randnum = new Random();
+
+    for (int i = 0; i < tmpArray.Length; i++)
+    {
+        tmpArray[i] = randnum.Next(tmpMin, tmpMax+1);
+    };
+
+    return tmpArray;
+}
+
+void PrintArray(int[] tmpArray)
+{
+    Console.WriteLine($"[" + string.Join(", ", tmpArray) + "]");
+}
+
+int SumOddPositionElements(int[] tmpArray)
+{
+    int tmpSum = 0;
+    for (int i = 1; i < tmpArray.Length; i+=2)
+    {
+        tmpSum = tmpSum+ tmpArray[i];
+    }
+    return tmpSum;
+}
+
+int len = ReadInt("Введите длину массива:");
+int[] array = FillArrayRandom(len, -255, 255);
+PrintArray(array);
+Console.WriteLine(SumOddPositionElements(array));
---
+++ task-38/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+*/
+
+int ReadInt(string text) 
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[] FillArrayRandom(int tmpLength, double tmpMin, double tmpMax)
+{
+    double[] tmpArray = new double[tmpLength];
+    Random randnum = new Random();
+
+    for (int i = 0; i < tmpArray.Length; i++)
+    {
+        tmpArray[i] = randnum.Next(tmpMin, tmpMax+1);
+    };
+
+    return tmpArray;
+}
+
+void PrintArray(int[] tmpArray)
+{
+    Console.WriteLine($"[" + string.Join(", ", tmpArray) + "]");
+}
+
+double FindMax(double[] tmpArray)
+{
+    double tmpMax = tmpArray[0];
+    for (int i = 1; i < tmpArray.Length; i++)
+    {
+        if (tmpArray[i] > tmpMax) tmpMax = tmpArray[i];
+    }
+    return tmpMax;
+}
+
+double FindMin(double[] tmpArray)
+{
+    double tmpMin = tmpArray[0];
+    for (int i = 1; i < tmpArray.Length; i++)
+    {
+        if (tmpArray[i] < tmpMin) tmpMin = tmpArray[i];
+    }
+    return tmpMin;
+}
+
+int len = ReadInt("Введите длину массива:");
+double[] array = FillArrayRandom(len, 0, 100);
+PrintArray(array);
+double diff = FindMax(array) - FindMin(array);
+Console.WriteLine(diff);
